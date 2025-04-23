4f5d5e8a63f3f18b10d23d7e42144a6fbdcf894b
sakaaw-sketch <wsurmun002@gmail.com>
2025-04-24 01:19:14 +0800

    Add files via upload

---
+++ problem/problem1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="/favicon.ico"/>
+<link href="/favicon.ico" rel="icon" type="image/x-icon"/>
+<link rel="icon" href="/favicon.ico"/>
+<link rel="bookmark" href="/favicon.ico"/>
+ <script type="text/javascript" src="http://acgms46.hopto.org/home/js/zzzphone.js"></script>
+<meta name="google-site-verification" content="CjbbA6KxN9_KN_sk3sHuIIV-ueCni8b0dmj4m5f63jw" />
+<title>problem1</title>
+<body background="http://acgms46.hopto.org/cover/flower2.gif">
+
+</head>
+<body> 
+      <center >
+<font color="#ff33cc">已載入指令碼</font>
+</center>
+
+
+
+
+						
+		
+			</body>
+			
+</html>
