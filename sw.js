c8881ee17341de386a20b470b61614252def0180
sakaaw-sketch <sakaouo2016@gmail.com>
2025-10-03 17:01:15 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'reception-v1';
+const urlsToCache = [
+  '/',
+  '/css/index2.css',
+  '/img/QR_code.png',
+  '/img/dSF3zNo.gif',
+  '/favicon.ico'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
